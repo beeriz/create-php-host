4906cefbbd4250ff684294381ffcb5d4d7cb1e9a
Mohamed amine Reguig <beeriz52@gmail.com>
2018-04-07 19:27:14 +0100

    Update create_php_site.sh

--- create_php_site.sh
+++ create_php_site.sh
@@ -6,9 +6,9 @@
 
 # Modify the following to match your system
 NGINX_CONFIG='/etc/nginx/sites-available'
-NGINX_SITES_ENABLED='/etc/nginx/sites-enabled'
+NGINX_SITES_ENABLED='/etc/nginx/conf.d/'
 PHP_INI_DIR='/etc/php/7.0/fpm/pool.d'
-WEB_SERVER_GROUP='www-data'
+WEB_SERVER_GROUP='nginx'
 NGINX_INIT='/etc/init.d/nginx'
 PHP_FPM_INIT='/etc/init.d/php7.0-fpm'
 # --------------END
@@ -58,8 +58,8 @@ fi
 CONFIG=$NGINX_CONFIG/$DOMAIN.conf
 cp $CURRENT_DIR/nginx.vhost.conf.template $CONFIG
 $SED -i "s/@@HOSTNAME@@/$DOMAIN/g" $CONFIG
-$SED -i "s#@@PATH@@#\/home\/"$USERNAME$PUBLIC_HTML_DIR"#g" $CONFIG
-$SED -i "s/@@LOG_PATH@@/\/home\/$USERNAME\/_logs/g" $CONFIG
+$SED -i "s#@@PATH@@#\/home\/main\/"$USERNAME$PUBLIC_HTML_DIR"#g" $CONFIG
+$SED -i "s/@@LOG_PATH@@/\/home\/main\/$USERNAME\/_logs/g" $CONFIG
 $SED -i "s#@@SOCKET@@#/var/run/"$USERNAME"_fpm.sock#g" $CONFIG
 
 echo "How many FPM servers would you like by default:"
@@ -74,7 +74,7 @@ FPMCONF="$PHP_INI_DIR/$DOMAIN.pool.conf"
 cp $CURRENT_DIR/pool.conf.template $FPMCONF
 
 $SED -i "s/@@USER@@/$USERNAME/g" $FPMCONF
-$SED -i "s/@@HOME_DIR@@/\/home\/$USERNAME/g" $FPMCONF
+$SED -i "s/@@HOME_DIR@@/\/home\/main\/$USERNAME/g" $FPMCONF
 $SED -i "s/@@START_SERVERS@@/$FPM_SERVERS/g" $FPMCONF
 $SED -i "s/@@MIN_SERVERS@@/$MIN_SERVERS/g" $FPMCONF
 $SED -i "s/@@MAX_SERVERS@@/$MAX_SERVERS/g" $FPMCONF
@@ -82,20 +82,22 @@ MAX_CHILDS=$((MAX_SERVERS+START_SERVERS))
 $SED -i "s/@@MAX_CHILDS@@/$MAX_CHILDS/g" $FPMCONF
 
 usermod -aG $USERNAME $WEB_SERVER_GROUP
-chmod g+rx /home/$HOME_DIR
+chmod g+rx /home/main/$HOME_DIR
 chmod 600 $CONFIG
 
 ln -s $CONFIG $NGINX_SITES_ENABLED/$DOMAIN.conf
 
 # set file perms and create required dirs!
-mkdir -p /home/$HOME_DIR$PUBLIC_HTML_DIR
-mkdir /home/$HOME_DIR/_logs
-mkdir /home/$HOME_DIR/_sessions
-chmod 750 /home/$HOME_DIR -R
-chmod 700 /home/$HOME_DIR/_sessions
-chmod 770 /home/$HOME_DIR/_logs
-chmod 750 /home/$HOME_DIR$PUBLIC_HTML_DIR
-chown $USERNAME:$USERNAME /home/$HOME_DIR/ -R
+mkdir -p /home/main/$HOME_DIR$PUBLIC_HTML_DIR
+mkdir /home/main/$HOME_DIR/_logs
+mkdir /home/main/$HOME_DIR/_sessions
+chmod 750 /home/main/$HOME_DIR -R
+chmod 700 /home/main/$HOME_DIR/_sessions
+chmod 770 /home/main/$HOME_DIR/_logs
+chmod 750 /home/main/$HOME_DIR$PUBLIC_HTML_DIR
+wget https://fr.wordpress.org/wordpress-4.9.5-fr_FR.zip -O /home/main/$HOME_DIR/wordpress-4.9.5-fr_FR.zip 
+unzip /home/main/$HOME_DIR/wordpress-4.9.5-fr_FR.zip -d /home/main/$HOME_DIR/public_html/
+chown $USERNAME:$USERNAME /home/main/$HOME_DIR/ -R
 
 $NGINX_INIT reload
 $PHP_FPM_INIT restart
